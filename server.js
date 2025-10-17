646cb3cde5aa94afd9a2b5bf193bfb3fd84378de
Antony <antonyth1818@gmail.com>
2025-10-18 02:55:12 +0530

    Phase-2-Event-Driven Architecture

--- server.js
+++ server.js
@@ -4,41 +4,69 @@ const { encode } = require("./protocol/encoder");
 
 const store = {};
 
-const server = net.createServer(connection => {
+const clients = new Set()
+
+const server = net.createServer(socket => {
+    socket.setNoDelay(true);
+    socket.setEncoding("utf8");
+    clients.add(socket);
     console.log("Client connected...");
 
-    connection.on("data", data => {
-        const [cmd, ...reply] = parseRESP(data);
+    socket.on("close", () => {
+        console.log("Client disconnected");
+        clients.delete(socket);
+    });
 
-        try {
-            switch (cmd.toLowerCase()) {
-                case "set": {
-                    const key = reply[0];
-                    const value = reply[1];
-                    store[key] = value;
-                    connection.write(encode("OK"));
-                    break;
-                }
-                case "get": {
-                    const key = reply[0];
-                    const value = store[key];
-                    if (value === undefined) {
-                        connection.write(encode(null));
-                    } else {
-                        connection.write(encode(value));
-                    }
-                    break;
-                }
-                default:
-                    connection.write(encode(new Error("Unknown command")));
-            }
-        } catch(err) {
-            connection.write(encode(err));
-        }     
+    socket.on("data", data => {
+            
     });
 });
 
 server.listen(8000, () => {
     console.log("Custom Redis server running on port 8000");
 });
+
+function eventLoop() {
+    for (const socket of clients) {
+        let data;
+        try {
+            data = socket.read();
+        } catch {
+            continue;
+        }
+        if(!data) continue;
+
+        try {
+            const [cmd, ...reply] = parseRESP(Buffer.from(data));
+
+            switch (cmd.toLowerCase()) {
+                case "set": {
+                    const key = reply[0];
+                    const value = reply[1];
+                    store[key] = value;
+                    socket.write(encode("OK"));
+                    break;
+                }
+                case "get": {
+                    const key = reply[0];
+                    const value = store[key];
+                    if (value === undefined) {
+                        socket.write(encode(null));
+                    } else {
+                        socket.write(encode(value));
+                    }
+                    break;
+                }
+                default:
+                    socket.write(encode(new Error("Unknown command")));
+            }
+        } catch(err) {
+            socket.write(encode(err));
+        }
+    }
+
+    setImmediate(eventLoop)
+}
+
+setImmediate(eventLoop)
 
