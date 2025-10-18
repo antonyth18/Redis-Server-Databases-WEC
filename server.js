bb5900c95cba1757306bded5b085a9771efe5b7b
Antony <antonyth1818@gmail.com>
2025-10-18 14:49:50 +0530

    Event-driven architecture fix

--- server.js
+++ server.js
@@ -1,50 +1,58 @@
 const net = require("net");
+const EventLoop = require("./eventLoop");
 const { parseRESP } = require("./protocol/parser");
 const { encode } = require("./protocol/encoder");
-const EventLoop = require("./eventLoop");
 
 const store = {};
-const buffers = new Map();
 const loop = new EventLoop();
+const clients = new Map();
 
 const server = net.createServer(socket => {
     console.log("Client connected");
-    buffers.set(socket, "");
+    socket.setEncoding("utf8");
 
-    loop.onReadable(socket, () => {
-        socket.on("data", chunk => {
-            let buffer = buffers.get(socket) + chunk;
-            try {
-                const [cmd, ...args] = parseRESP(Buffer.from(buffer));
-                handleCommand(socket, cmd, args);
-                buffers.set(socket, "");
-            } catch {
-                buffers.set(socket, buffer);
-            }
-        });
+    clients.set(socket, { buffer: "" });
 
-        socket.on("close", () => {
-            buffers.delete(socket);
-        });
+    socket.on("data", data => {
+        clients.get(socket).buffer += data;
     });
-});
 
-function handleCommand(socket, cmd, args) {
-    switch (cmd.toLowerCase()) {
-        case "set":
-            store[args[0]] = args[1];
-            socket.write(encode("OK"));
-            break;
-        case "get":
-            socket.write(encode(store[args[0]] || null));
-            break;
-        default:
-            socket.write(encode(new Error("Unknown command")));
-    }
-}
+    socket.on("close", () => {
+        console.log("Client disconnected");
+        clients.delete(socket);
+    });
+
+    loop.onReadable(socket, handleClient);
+});
 
 server.listen(8000, () => {
     console.log("Custom Redis server running on port 8000");
-    loop.run();
 });
+
+function handleClient(socket) {
+    const client = clients.get(socket);
+    if (!client || client.buffer.length === 0) return;
+
+    const data = client.buffer;
+    client.buffer = "";
+
+    try {
+        const [cmd, ...args] = parseRESP(Buffer.from(data));
+        switch (cmd.toLowerCase()) {
+            case "set":
+                store[args[0]] = args[1];
+                socket.write(encode("OK"));
+                break;
+            case "get":
+                socket.write(encode(store[args[0]] ?? null));
+                break;
+            default:
+                socket.write(encode(new Error("Unknown command")));
+        }
+    } catch (err) {
+        socket.write(encode(err));
+    }
+}
+
+loop.run();
 
